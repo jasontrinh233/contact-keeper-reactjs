2555011af505df6441cc8f8cd53651bd22acc006
Quang Trinh <jasontrinh233@gmail.com>
2019-06-18 11:03:26 -0700

    Add Contacts to specific User

--- routes/contacts.js
+++ routes/contacts.js
@@ -25,9 +25,41 @@ router.get("/", auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post("/", (req, res) => {
-   res.send("Add contact");
-});
+router.post(
+   "/",
+   [
+      auth,
+      [
+         check("name", "Name is required")
+            .not()
+            .isEmpty()
+      ]
+   ],
+   async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ msg: errors.array() });
+      }
+
+      const { name, email, phone, type } = req.body;
+
+      try {
+         const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+         });
+
+         const contact = await newContact.save();
+         return res.status(201).json(contact);
+      } catch (err) {
+         console.error(err.message);
+         return res.status(500).json({ msg: "Server error" });
+      }
+   }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
