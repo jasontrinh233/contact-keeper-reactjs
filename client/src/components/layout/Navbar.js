35ab808230564152160535f85ff4eeb311966b44
Quang Trinh <jasontrinh233@gmail.com>
2019-06-20 00:07:28 -0700

    Logout user

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,47 +1,50 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 import AuthContext from "../../context/auth/authContext";
 
-const Navbar = ({ title, icon }) => {
+const Navbar = ({ icon, title }) => {
    const authContext = useContext(AuthContext);
 
-   const { user } = authContext;
+   const { user, isAuthenticated, logout } = authContext;
 
-   if (!user) {
-      return (
-         <div className="navbar bg-primary">
-            <h1>
-               <i className={icon} /> {title}
-            </h1>
-            <ul>
-               <li>
-                  <Link to="/register">Register</Link>
-               </li>
-               <li>
-                  <Link to="/login">Login</Link>
-               </li>
-            </ul>
-         </div>
-      );
-   } else {
-      return (
-         <div className="navbar bg-primary">
-            <h1>
-               <i className={icon} /> {title}
-            </h1>
-            <ul>
-               <li>
-                  <Link to="/">Home</Link>
-               </li>
-               <li>
-                  <Link to="/about">About</Link>
-               </li>
-            </ul>
-         </div>
-      );
-   }
+   // onLogout hander
+   const onLogout = () => {
+      logout();
+   };
+
+   const authLinks = (
+      <Fragment>
+         <li>Hello {user && user.name}</li>
+         <li>
+            <a onClick={onLogout} href="#!">
+               <i className="fas fa-sign-out-alt"></i>{" "}
+               <span className="hide-sm">Logout</span>
+            </a>
+         </li>
+      </Fragment>
+   );
+
+   const guestLinks = (
+      <Fragment>
+         <li>
+            <Link to="/register">Register</Link>
+         </li>
+         <li>
+            <Link to="/login">Login</Link>
+         </li>
+      </Fragment>
+   );
+
+   return (
+      <div className="navbar bg-primary">
+         <h1>
+            <i className={icon} /> {title}
+         </h1>
+         <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+      </div>
+   );
 };
 
 // Define prop types
